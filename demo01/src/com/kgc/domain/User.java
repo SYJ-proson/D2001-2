a39718c026d01e250ae4d93aa847800a349fa3f2
SYJ <1980410102@qq.com>
2020-08-28 11:40:18 +0800

    提交

---
+++ demo01/src/com/kgc/domain/User.java
@@ -0,0 +1,12 @@
+package com.kgc.domain;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+	
+	public static void main(String[] args) {
+		System.out.println("hhh");
+		
+	}
+
+}
